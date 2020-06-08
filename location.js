de410289a26dceb5117abfe328b4d47a04da71ab
韩林楠 <80003979@sfmail.sf-express.com>
2020-06-08 16:44:40 +0800

    1

--- location.js
+++ location.js
@@ -35,69 +35,22 @@ function getlocationpoint() {
         alert("不支持定位功能");
     }
 }
-
-function getIPs(callback) {
-    var ip_dups = {};
-    //compatibility for firefox and chrome
-    var RTCPeerConnection = window.RTCPeerConnection
-        || window.mozRTCPeerConnection
-        || window.webkitRTCPeerConnection;
-    //bypass naive webrtc blocking
-    if (!RTCPeerConnection) {
-        var iframe = document.createElement("iframe");
-        //invalidate content script
-        iframe.sandbox = "allow-same-origin";
-        iframe.style.display = "none";
-        document.body.appendChild(iframe);
-        var win = iframe.contentWindow;
-        window.RTCPeerConnection = win.RTCPeerConnection;
-        window.mozRTCPeerConnection = win.mozRTCPeerConnection;
-        window.webkitRTCPeerConnection = win.webkitRTCPeerConnection;
-        RTCPeerConnection = window.RTCPeerConnection
-            || window.mozRTCPeerConnection
-            || window.webkitRTCPeerConnection;
+function GetLocalIPAddr() {
+    var oSetting = null;
+    var ip = null;
+    try {
+      oSetting = new ActiveXObject("rcbdyctl.Setting");
+      ip = oSetting.GetIPAddress;
+      if (ip.length == 0) {
+        return "没有连接到Internet";
+      }
+      oSetting = null;
+    } catch (e) {
+      return ip;
     }
-    var mediaConstraints = {
-        optional: [{ RtpDataChannels: true }]
-    };
-    //firefox already has a default stun server in about:config
-    // media.peerconnection.default_iceservers =
-    // [{"url": "stun:stun.services.mozilla.com"}] var servers = undefined;
-    //add same stun server for chrome
-    if (window.webkitRTCPeerConnection)
-        var servers = { iceServers: [{ urls: "stun:stun.services.mozilla.com" }] };
-    //construct a new RTCPeerConnection
-    var pc = new RTCPeerConnection(servers, mediaConstraints);
-    //listen for candidate events
-    pc.onicecandidate = function (ice) {
-        //skip non-candidate events
-        if (ice.candidate) {
-            //match just the IP address
-            var ip_regex = /([0-9]{1,3}(\.[0-9]{1,3}){3})/
-            var ip_addr = ip_regex.exec(ice.candidate.candidate)[1];
-            //remove duplicates
-            if (ip_dups[ip_addr] === undefined)
-                callback(ip_addr);
-            ip_dups[ip_addr] = true;
-        }
-    };
-    //create a bogus data channel
-    pc.createDataChannel("");
-    //create an offer sdp
-    pc.createOffer(function (result) {
-        //trigger the stun server request
-        pc.setLocalDescription(result, function () { }, function () { });
-    }, function () { });
-}
-getIPs(function (ip) {
-    alert("本机IP地址为:"+ ip);
-});
+    alert(ip);
+    return ip;
+  }
 
-function plusReady() {
-    alert("IMEI: " + plus.device.imei);
-};
-if(window.plus){  
-    plusReady();  
-    }else{  
-    document.addEventListener("plusready",plusReady,false);  
-    }  
+
+    
